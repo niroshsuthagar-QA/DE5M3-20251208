67501556aaa9035beaed2331e3463c04a9e5ee5f
niroshsuthagar-QA <nirosh.suthagar@qa.com>
2025-12-09 09:08:58 +0000

    Add files via upload

---
+++ createDW.ps1
@@ -0,0 +1,18 @@
+﻿#This script will create/recreate the QASQLETLDW Database
+
+#Create the folders to store the .mdf and .ldf files
+
+if (-not (test-path -Path f:\sqletldb -PathType Container))
+{
+    New-Item -Path f:\sqletldb -ItemType Directory
+}
+
+
+if (-not (test-path -Path g:\sqletlLogs -PathType Container))
+{
+    New-Item -Path g:\sqletlLogs -ItemType Directory
+}
+
+#Run a SQL script to Create the Data Warehouse
+
+Invoke-Sqlcmd -InputFile '.\create DW Script.sql'
